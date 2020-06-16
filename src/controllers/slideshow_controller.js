d37756a90ac36bda70c0f3bfa41098d93355699a
Rachel Bingham <eddie901@gmail.com>
2020-06-16 15:39:48 +0100

    Using the data attribute get and set methods

--- src/controllers/slideshow_controller.js
+++ src/controllers/slideshow_controller.js
@@ -6,36 +6,34 @@ export default class extends Controller {
 
 
     initialize() {
-        const index = parseInt(this.data.get("index"))
-        this.showSlide(index)
-        console.log(`Index is ${this.index}`)
-
+        this.showCurrentSlide()
     }
 
     next() {
-        this.showSlide(this.index + 1)
-        console.log(`Index is, ${this.index}`)
-
+        this.index++
     }
 
     previous() {
-        this.showSlide(this.index - 1)
-        console.log(`Index is, ${this.index}`)
-
+        this.index--
     }
 
-    showSlide(index) {
-        this.index = index
-        // here's what I think the method toggle(token, boolean) is doing
-        // if the token is in the element's classlist, it toggles it
-        // and if it doesn't but boolean is true it applies it
-        // so for the current slide slide--current class is removed
-        // and for the target slide it is applied
-        // for the other slides there is no effect since no token and boolean is false
-        // BUT THIS IS PROBABLY THE WRONG EXPLANATION
-        // How are the other slides being hidden???
+    showCurrentSlide() {
+        console.log(`Index is ${this.index}`)
+
         this.slideTargets.forEach((el, i) => {
-            el.classList.toggle("slide--current", index == i)
+            el.classList.toggle("slide--current", this.index == i)
         })
+    }
+
+    // when does the get method get called?
+    // everytime the this.index is referenced??
+    get index() {
+        return parseInt(this.data.get("index"))
+    }
+
+    // so this.index++ call the setter automatically?
+    set index(value) {
+        this.data.set("index", value)
+        this.showCurrentSlide()
     }
 }
