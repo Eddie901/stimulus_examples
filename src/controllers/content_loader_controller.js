6dfa47765a7fccc105195348afa92c9dada7e058
rachelb <rachelb@bloomandwild.com>
2020-06-17 11:49:13 +0100

    Add refresh timer to content loader

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -4,6 +4,10 @@ import { Controller } from "stimulus"
 export default class extends Controller {
     connect() {
         this.load()
+
+        if (this.data.has("refreshInterval")) {
+            this.startRefreshing()
+        }
     }
 
     load() {
@@ -12,5 +16,21 @@ export default class extends Controller {
             .then(html => {
                 this.element.innerHTML = html
             })
+    }
+
+    disconnect() {
+        this.stopRefreshing()
+    }
+
+    startRefreshing() {
+        this.refreshTimer = setInterval(() => {
+            this.load()
+        }, this.data.get("refreshInterval"))
+    }
+
+    stopRefreshing() {
+        if (this.refreshTimer) {
+            clearInterval(this.refreshTimer)
+        }
     }
 }
